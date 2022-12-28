7b4799cda3176ce6ca35db9af4af9b03a8b15054
Aparna <Aparna@NTP-LAP-676>
2022-12-28 19:31:18 +0530

    Add project files.

---
+++ BackEnd/Data/DataContextClass.cs
@@ -0,0 +1,20 @@
+﻿using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Data
+{
+    public class DataContextClass : DbContext
+    {
+        public DataContextClass(DbContextOptions<DataContextClass> options) : base(options)
+        {
+
+
+
+        }
+        public DbSet<Registration> tblregistration { get; set; }
+    }
+}
